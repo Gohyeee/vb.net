0b82bc83a7fe119da4657b1f5e27fb92b2343ae2
mimi <130416946+hyhyhyee@users.noreply.github.com>
2023-06-20 14:38:32 +0900

    [Feat] : 땜쓰08

---
+++ ex08/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    'Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '    Form2.Show()
+    'End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = TextBox1.Text And TextBox2.Text
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox3.Text = TextBox1.Text Or TextBox2.Text
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox3.Text = TextBox1.Text Xor TextBox2.Text
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox3.Text = TextBox1.Text AndAlso TextBox2.Text
+    End Sub
+
+
+
+End Class
---
+++ ex08/Form2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label3.Text = Label3.Text + " " + TextBox1.Text + "입니다."
+        Label4.Text = Label4.Text + " " + Val(TextBox2.Text) * 0.01 & "원 입니다."
+    End Sub
+End Class
